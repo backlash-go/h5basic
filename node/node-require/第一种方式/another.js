cccbafe27756e5062e7f10f9548cb2edf45d77f4
backlash <xixianbing@hexiaoxiang.com>
2021-04-20 20:04:58 +0800

    vue

---
+++ node/node-require/第一种方式/another.js
@@ -0,0 +1,5 @@
+const car = require('./car')
+
+
+console.log(car.car.brand)
+console.log(car.xxb.age)
---
+++ node/node-require/第一种方式/car.js
@@ -0,0 +1,18 @@
+const car = {
+    brand: "ford",
+    model: 'Fiesta'
+}
+// //第一种
+// module.exports = car
+
+
+
+
+const xxb = {
+    name: "bob",
+    age: 18
+}
+
+exports.xxb = xxb
+exports.car = car
+
