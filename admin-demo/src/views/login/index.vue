eb2e78e82bfacbebae15ae104ebcd1c85167b1ba
xixb <xixianbing@hexiaoxiang.com>
2021-04-24 21:36:20 +0800

    ele

--- admin-demo/src/views/login/index.vue
+++ admin-demo/src/views/login/index.vue
@@ -1,10 +1,38 @@
 <template>
-<div class="login-container">登陆页面</div>
-</template>
+<el-form ref="form" :model="form" label-width="80px">
+  <el-form-item label="活动名称">
+    <el-input v-model="form.name"></el-input>
+  </el-form-item>
+
+
+  <el-form-item>
+    <el-button type="primary" @click="onSubmit">立即创建</el-button>
+    <el-button>取消</el-button>
+  </el-form-item>
+</el-form></template>
 
 <script>
 export default {
-name: "LoginIndex"
+name: "LoginIndex",
+data() {
+      return {
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        }
+      }
+    },
+ methods: {
+      onSubmit() {
+        console.log('submit!');
+      }
+    }   
 }
 </script>
 
