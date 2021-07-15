94a5b77303a979cb4d9a36271bc0b72c3dd55017
backlash <xixianbing@hexiaoxiang.com>
2021-07-15 22:54:07 +0800

    add dom

--- jirengu/DOM-1/src/dom.js
+++ jirengu/DOM-1/src/dom.js
@@ -10,21 +10,73 @@ window.dom = {
         node.parentNode.insertBefore(node2, node.nextSibling);
     },
     before(node, node2) {
-        node.parentNode.insertBefore(node2, node)
+        node.parentNode.insertBefore(node2, node);
     },
     append(parent, node) {
-        parent.appendChild(node)
+        parent.appendChild(node);
     },
     wrap(node, parent) {
-        dom.before(node, parent)
-        dom.append(parent, node)
+        dom.before(node, parent);
+        dom.append(parent, node);
     },
     remove(node) {
         node.parentNode.removeChild(node);
-        return node
+        return node;
     },
     empty(node) {
-        const {childNodes} = node
-        console.log(childNodes)
+        const {childNodes} = node;
+        console.log(childNodes);
+        const array = [];
+        let x = node.firstChild;
+        console.log(x);
+        while (x) {
+            array.push(dom.remove(node.firstChild));
+            x = node.firstChild;
+        }
+        return array;
+
+    },
+    attr(node, name, value) {  //重载
+        if (arguments.length === 3) {
+            node.setAttribute(name, value);
+        } else if (arguments.length === 2) {
+            return node.getAttribute(name);
+        }
+    },
+    text(node, string) { //适配
+        if (arguments.length === 2) {
+            if ('innerText' in node) {
+                node.innerText = string;
+            } else if ('textContent' in node) {
+                node.textContent = string;
+            }
+        } else if (arguments.length === 1) {
+            if ('innerText' in node) {
+                return node.innerText;
+            } else if ('textContent' in node) {
+                return node.textContent;
+            }
+        }
+
+    },
+    style(node, name, value) {
+        if (arguments.length === 3) {
+            //dom.style(div,color,red)
+            node.style[name] = value;
+        } else if (arguments.length === 2) {
+            //dom.style(div,color)
+
+            if (typeof name === 'string') {
+                return node.style[name];
+            }
+        } else if (name instanceof Object) {
+            //dom.style(div,object)
+            for (let key in name) {
+                node.style[key] = name[key];
+            }
+        }
+
+
     }
-}
+
+};
--- jirengu/DOM-1/src/main.js
+++ jirengu/DOM-1/src/main.js
@@ -1,11 +1,9 @@
-
-
 //
 //
 
-const  d1 = document.querySelector('#test')
-
-const  div3 = dom.create("<div>222</div>")
+const test = document.querySelector('#test');
+console.log(test);
+const div3 = dom.create("<div>222</div>");
 
 // dom.wrap(div3,d1)
 // console.log(d1.parentNode)
@@ -14,5 +12,18 @@ const  div3 = dom.create("<div>222</div>")
 
 // dom.append(d1,div)
 // dom.before(div3,d1)
-dom.empty(d1)
+// dom.empty(d1)
 
+const nodes = dom.empty(window.empty);
+
+dom.attr(test, "title", "i am xxb1");
+console.log(nodes);
+
+const title = dom.attr(test, "title");
+console.log(`title is ${title}`);
+
+
+dom.text(test, "我是大家的小宝贝");
+
+dom.style(test, {border: '1px solid red', color: "blue"});
+dom.style(test, "border");
