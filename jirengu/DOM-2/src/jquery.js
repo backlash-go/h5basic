50d84a3cf72513814c250821cc8860af88ebfbbe
backlash <xixianbing@hexiaoxiang.com>
2021-07-17 10:46:55 +0800

    jq up

--- jirengu/DOM-2/src/jquery.js
+++ jirengu/DOM-2/src/jquery.js
@@ -1,21 +1,51 @@
-window.jQuery = function (selector) {
-    const elements = document.querySelectorAll(selector)
+window.jQuery = function (selectorOrArray) {
+    let elements;
+    if (typeof selectorOrArray === 'string') {
+        elements = document.querySelectorAll(selectorOrArray);
+    } else if (selectorOrArray instanceof Array) {
+        elements = selectorOrArray;
+    }
+
     return {
+        oldApi: selectorOrArray.oldApi,
         addClass(className) {
             for (let i = 0; i < elements.length; i++) {
-                elements[i].classList.add(className)
+                elements[i].classList.add(className);
             }
-            return this
+            return this;
             // return api
         },
         find(selector) {
-            let array = []
+            let array = [];
             for (let i = 0; i < elements.length; i++) {
-                array.concat(Array.from(elements[i]))
+                const elements2 = Array.from(elements[i].querySelectorAll(selector));
+                array = array.concat(elements2);
             }
-            return array
-        }
-    }
-}
+            array.oldApi = this; //  this is api1
+            return jQuery(array);
+        },
+        end() {
+            return this.oldApi;
+        },
+        each(fn) {
+            for (let i = 0; i < elements.length; i++) {
+                fn.call(null, elements[i], i);
+            }
+            return this;
+        },
+        parent() {
+            let array = [];
+            this.each((node) => {
+                if (array.indexOf(node.parentNode) === -1) {
+                    array.push(node.parentNode());
+                }
+            });
+            return jQuery(array);
+        },
+        print() {
+            console.log(elements);
+        },
+    };
+};
 
 /* obj.fn(p1)  obj.fn.call(obj,p1) */
--- jirengu/DOM-2/src/main.js
+++ jirengu/DOM-2/src/main.js
@@ -3,6 +3,19 @@
 // api.addClass("red").addClass("xxb1") //链式操作
 
 
+// const x = jQuery(".test1").find(".child")
+// console.log(x)
 
-const x = jQuery(".test1").find(".child")
-console.log(x)
+const api1 = jQuery('.test');
+api1.addClass('blue');
+console.log(api1)
+
+const api2 = api1.find('.child').addClass('red').end().addClass('yellow');
+console.log(api2)
+
+if (api1 !== api2 ){
+    console.log("api1 !== api2")
+}
+
+api1.addClass('green')
+
