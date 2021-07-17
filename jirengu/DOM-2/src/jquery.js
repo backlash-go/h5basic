bfa7d4031c39efccae26d5dcecd9d2984883f685
backlash <xixianbing@hexiaoxiang.com>
2021-07-17 23:38:32 +0800

    add jq

--- jirengu/DOM-2/src/jquery.js
+++ jirengu/DOM-2/src/jquery.js
@@ -1,51 +1,59 @@
-window.jQuery = function (selectorOrArray) {
+window.$ = window.jQuery = function (selectorOrArray) {
     let elements;
     if (typeof selectorOrArray === 'string') {
         elements = document.querySelectorAll(selectorOrArray);
     } else if (selectorOrArray instanceof Array) {
         elements = selectorOrArray;
     }
-
-    return {
+    const api = Object.create(jQuery.prototype);
+    Object.assign(api, {
         oldApi: selectorOrArray.oldApi,
-        addClass(className) {
-            for (let i = 0; i < elements.length; i++) {
-                elements[i].classList.add(className);
-            }
-            return this;
-            // return api
-        },
-        find(selector) {
-            let array = [];
-            for (let i = 0; i < elements.length; i++) {
-                const elements2 = Array.from(elements[i].querySelectorAll(selector));
-                array = array.concat(elements2);
-            }
-            array.oldApi = this; //  this is api1
-            return jQuery(array);
-        },
-        end() {
-            return this.oldApi;
-        },
-        each(fn) {
-            for (let i = 0; i < elements.length; i++) {
-                fn.call(null, elements[i], i);
-            }
-            return this;
-        },
-        parent() {
-            let array = [];
-            this.each((node) => {
-                if (array.indexOf(node.parentNode) === -1) {
-                    array.push(node.parentNode());
-                }
-            });
-            return jQuery(array);
-        },
-        print() {
-            console.log(elements);
-        },
-    };
+        elements: elements,
+    });
+    console.log(api)
+    return api;
 };
 
 /* obj.fn(p1)  obj.fn.call(obj,p1) */
+
+
+jQuery.prototype = {
+    jQuery: true,
+    addClass(className) {
+        for (let i = 0; i < this.elements.length; i++) {
+            this.elements[i].classList.add(className);
+        }
+        return this;
+        // return api
+    },
+    find(selector) {
+        let array = [];
+        for (let i = 0; i < elements.length; i++) {
+            const elements2 = Array.from(elements[i].querySelectorAll(selector));
+            array = array.concat(elements2);
+        }
+        array.oldApi = this; //  this is api1
+        return jQuery(array);
+    },
+    end() {
+        return this.oldApi;
+    },
+    each(fn) {
+        for (let i = 0; i < elements.length; i++) {
+            fn.call(null, elements[i], i);
+        }
+        return this;
+    },
+    parent() {
+        let array = [];
+        this.each((node) => {
+            if (array.indexOf(node.parentNode) === -1) {
+                array.push(node.parentNode());
+            }
+        });
+        return jQuery(array);
+    },
+    print() {
+        console.log(elements);
+    },
+};
--- jirengu/DOM-2/src/main.js
+++ jirengu/DOM-2/src/main.js
@@ -6,16 +6,16 @@
 // const x = jQuery(".test1").find(".child")
 // console.log(x)
 
-const api1 = jQuery('.test');
+const api1 = $('.test');
 api1.addClass('blue');
-console.log(api1)
-
-const api2 = api1.find('.child').addClass('red').end().addClass('yellow');
-console.log(api2)
-
-if (api1 !== api2 ){
-    console.log("api1 !== api2")
-}
-
-api1.addClass('green')
+// console.log(api1)
+//
+// const api2 = api1.find('.child').addClass('red').end().addClass('yellow');
+// console.log(api2)
+//
+// if (api1 !== api2 ){
+//     console.log("api1 !== api2")
+// }
+//
+// api1.addClass('green')
 
